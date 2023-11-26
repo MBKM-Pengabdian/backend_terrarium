3589e10a9962057ad818d9b88fd85b6d14fab0d2
MhmmdSyahputra <putra.cokers@gmail.com>
2023-11-26 19:28:03 +0700

    fix: fixit API event in update data event and getall data and getbyid

--- src/controllers/event/update.js
+++ src/controllers/event/update.js
@@ -5,45 +5,76 @@ const prisma = new PrismaClient();
 
 // Update a event
 export const updateEvent = async (req, res) => {
-    const {
-        eventId
-    } = req.params;
-    const {
-        user_id,
-        img_event,
-        banner_event,
-        title_event,
-        price_event,
-        tag_event,
-        kuota_event,
-        date_event,
-        last_regist_event,
-        sisa_event,
-        status,
-    } = req.body;
-
-    const updatedEvent = await prisma.event.update({
-        where: {
-            uuid: eventId
-        },
-        data: {
+    try {
+        const eventId = req.params.eventId;
+        const {
             user_id,
             img_event,
-            banner_event,
             title_event,
             price_event,
-            tag_event,
-            kuota_event,
-            date_event,
-            last_regist_event,
-            sisa_event,
             status,
-        },
-    });
+            detailEvent
+        } = req.body;
 
-    res.status(200).send({
-        status: 200,
-        message: "Updated",
-        data: updateEvent
-    });
+        const updatedEvent = await prisma.event.update({
+            where: {
+                uuid: eventId
+            },
+            data: {
+                user_id,
+                img_event,
+                title_event,
+                price_event,
+                status,
+                detail_event: {
+                    update: detailEvent.forEach(async detail => {
+                        await prisma.detail_Event.update({
+                            where: {
+                                id: detail.id
+                            },
+                            data: {
+                                description_event: detail.description_event,
+                                sponsor_event: detail.sponsor_event,
+                                speaker_event: detail.speaker_event,
+                                banner_event: detail.banner_event,
+                                tag_event: detail.tag_event,
+                                date_event: detail.date_event,
+                                last_regist_event: detail.last_regist_event,
+                                kuota_event: detail.kuota_event,
+                                sisa_event: detail.sisa_event,
+                                timeline: {
+                                    deleteMany: {},
+                                    createMany: {
+                                        data: detail.timeline.map(time => ({
+                                            title: time.title,
+                                            waktu: time.waktu,
+                                        })),
+                                    },
+                                },
+                            },
+                        });
+                    }),
+                },
+            },
+            include: {
+                detail_event: {
+                    include: {
+                        timeline: true,
+                    },
+                },
+            },
+        });
+
+        res.status(200).json({
+            status: 200,
+            message: 'Event updated successfully',
+            data: updatedEvent,
+        });
+    } catch (error) {
+        console.error('Error updating event:', error);
+        res.status(500).json({
+            status: 500,
+            message: 'Internal Server Error',
+        });
+    }
 };
