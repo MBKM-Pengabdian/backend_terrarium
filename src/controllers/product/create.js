359f9ec2c206453961462eeafd8fcc13086ddcc3
mhdusop <mhduusop@gmail.com>
2023-12-29 19:49:34 +0700

    refactor: change input file can be null chore; update schema product at colum product_image can be null

--- src/controllers/product/create.js
+++ src/controllers/product/create.js
@@ -38,31 +38,36 @@ export const createProduct = async (req, res) => {
 
       const { user_id, product_name, description, price, stock_quantity } = req.body;
 
-      const fileUrl = `/images/${req.file.filename}`;
+      let fileUrl = null
+
+      if (req.file) {
+         fileUrl = `/images/${req.file.filename}`;
+
+         if (req.file.size > config.IMG_LIMIT_SIZE) {
+            const size = (await fs.promises.stat(req.file.path)
+               .then((respond) => {
+                  return respond.size;
+               })
+               .catch((error) => {
+                  return -1;
+               }));
+
+            const sizeAcceptedInPercent = Math.floor((size - (size - (config.IMG_LIMIT_SIZE / 2))) / (size / 100));
+
+            await sharp(req.file.path)
+               .jpeg({ quality: sizeAcceptedInPercent })
+               .toFile(`${config.IMG_UPLOAD_DIR}/compress-${req.file.filename}`)
+               .then((res) => {
+                  fileUrl = `/images/compress-${req.file.filename}`;
+               })
+               .catch((err) => {
+                  console.error(err);
+               })
+         }
+      };
       const parsedPrice = parseFloat(price);
       const parsedStockQuantity = parseInt(stock_quantity)
 
-      if (req.file.size > config.IMG_LIMIT_SIZE) {
-         const size = (await fs.promises.stat(req.file.path)
-            .then((respond) => {
-               return respond.size;
-            })
-            .catch((error) => {
-               return -1;
-            }));
-
-         const sizeAcceptedInPercent = Math.floor((size - (size - (config.IMG_LIMIT_SIZE / 2))) / (size / 100));
-
-         await sharp(req.file.path)
-            .jpeg({ quality: sizeAcceptedInPercent })
-            .toFile(`${config.IMG_UPLOAD_DIR}/compress-${req.file.filename}`)
-            .then((res) => {
-               fileUrl = `/images/compress-${req.file.filename}`;
-            })
-            .catch((err) => {
-               console.error(err);
-            })
-      }
 
       const newProduct = await prisma.Product.create({
          data: {
