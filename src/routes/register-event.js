e315982ca0c21fa2dd5ada397ca924f4e759decc
mhdusop <mhduusop@gmail.com>
2024-03-26 11:26:15 +0700

    feat: add 1 endpoint for get all event registration and change response get register event user

--- src/routes/register-event.js
+++ src/routes/register-event.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { checkJWTCustomer } from "../middleware/jwt.js";
+import { checkJWTCustomer, checkJWTSuperAdmin, checkJWTAdmin } from "../middleware/jwt.js";
 import { registerEvent } from "./../controllers/register_event/create.js";
-import { getAllRegistrationEvent } from "./../controllers/register_event/read.js";
+import { getRegistrationEventUser, getAllRegistrationEvent } from "./../controllers/register_event/read.js";
 
 const RegisterEventRouter = express.Router();
 
-RegisterEventRouter.get('/register-event/:uuid_customer/get', checkJWTCustomer, getAllRegistrationEvent);
+RegisterEventRouter.get('/register-event/get', checkJWTSuperAdmin, checkJWTAdmin, getAllRegistrationEvent);
+RegisterEventRouter.get('/register-event/:uuid_customer/get', checkJWTCustomer, getRegistrationEventUser);
 RegisterEventRouter.post('/register-event/store', checkJWTCustomer, registerEvent);
 
 export default RegisterEventRouter;
