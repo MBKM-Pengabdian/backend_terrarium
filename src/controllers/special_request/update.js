1ca5a2a9175507955841209a9604245ecda0542e
MhmmdSyahputra <putra.cokers@gmail.com>
2024-03-27 17:39:07 +0700

    feat: add alasan in special request

--- src/controllers/special_request/update.js
+++ src/controllers/special_request/update.js
@@ -1,32 +1,34 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// Increase quantity 
+// Increase quantity
 export const updateStatusSpecialRequest = async (req, res) => {
-   const { specialReqID } = req.params;
-   const { status } = req.body
+  const { specialReqID } = req.params;
+  const { status, alasan } = req.body;
 
-   try {
-      const specialRequest = await prisma.special_Request.findUnique({
-         where: { uuid: specialReqID },
-      });
+  try {
+    const specialRequest = await prisma.special_Request.findUnique({
+      where: { uuid: specialReqID },
+    });
 
-      if (!specialRequest) {
-         return res.status(404).json({ error: 'Special Request not found' });
-      }
+    if (!specialRequest) {
+      return res.status(404).json({ error: "Special Request not found" });
+    }
 
-      // Increase quantity logic
-      specialRequest.status = status;
 
-      await prisma.special_Request.update({
-         where: { uuid: specialReqID },
-         data: { status: specialRequest.status },
-      });
+   const udpateSpecialReq = await prisma.special_Request.update({
+      where: { uuid: specialReqID },
+      data: { status: status, alasan: alasan},
+    });
 
-      res.json({ message: 'Status successfully Update', specialRequest });
-   } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-   }
+    res.json({
+      status: 200,
+      message: "Status successfully Update",
+      udpateSpecialReq,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
