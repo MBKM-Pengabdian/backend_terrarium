9f227e395e3b4e5442d51d811307714d80466f9c
MhmmdSyahputra <putra.cokers@gmail.com>
2024-03-27 11:30:32 +0700

    feat: add status special request and add route update status

---
+++ src/controllers/special_request/update.js
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+// Increase quantity 
+export const updateStatusSpecialRequest = async (req, res) => {
+   const { specialReqID } = req.params;
+   const { status } = req.body
+   console.log(status);
+
+   try {
+      const specialRequest = await prisma.special_Request.findUnique({
+         where: { uuid: specialReqID },
+      });
+
+      if (!specialRequest) {
+         return res.status(404).json({ error: 'Special Request not found' });
+      }
+
+      // Increase quantity logic
+      specialRequest.status = status;
+
+      await prisma.special_Request.update({
+         where: { uuid: specialReqID },
+         data: { status: specialRequest.status },
+      });
+
+      res.json({ message: 'Status successfully Update', specialRequest });
+   } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+   }
+};
--- src/routes/special-request.js
+++ src/routes/special-request.js
@@ -2,12 +2,14 @@ import express from "express";
 import { checkJWTAdmin, checkJWTSuperAdmin, checkJWTCustomer } from "../middleware/jwt.js";
 import { createSpecialRequest } from "../controllers/special_request/create.js";
 import { getAllSpecialReq, getAllSpecialReqFromUuidCustomer } from "../controllers/special_request/read.js";
+import { updateStatusSpecialRequest } from "../controllers/special_request/update.js";
 
 const specialRequestRoute = express.Router();
 
 specialRequestRoute.get('/get', checkJWTAdmin, checkJWTSuperAdmin, getAllSpecialReq);
 specialRequestRoute.get('/:uuid_customer/get', checkJWTCustomer, getAllSpecialReqFromUuidCustomer);
 specialRequestRoute.post('/store', checkJWTCustomer, createSpecialRequest);
+specialRequestRoute.post('/updateStatus/:specialReqID', checkJWTCustomer, updateStatusSpecialRequest)
 
 
 export default specialRequestRoute;
