37e0aec92edc51a1eb0eb2fb66f1a9a8f07ce7f3
MhmmdSyahputra <putra.cokers@gmail.com>
2024-04-24 16:49:52 +0700

    feat: fix return data customer

--- src/controllers/customers/read.js
+++ src/controllers/customers/read.js
@@ -1,54 +1,66 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 // Retrieve all customers
 export const getAllCustomers = async (req, res) => {
-   try {
-      const customers = await prisma.customer.findMany({
-         select: {
-            uuid: true,
-            username: true,
-            email: true,
-            password: true,
-            phone: true,
-            address: true,
-            created_at: true,
-            status_customer: true,
-         },
-         orderBy: {
-            created_at: 'asc'
-         }
-      });
-      res.status(200).send({
-         status: 200,
-         message: "OK",
-         data: customers
-      });
-   } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-   }
+  try {
+    const customers = await prisma.customer.findMany({
+      select: {
+        uuid: true,
+        username: true,
+        email: true,
+        password: true,
+        phone: true,
+        address: true,
+        created_at: true,
+        status_customer: true,
+      },
+      orderBy: {
+        created_at: "asc",
+      },
+    });
+    res.status(200).send({
+      status: 200,
+      message: "OK",
+      data: customers,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // Retrieve a single customer by UUID
 export const getCustomerById = async (req, res) => {
-   try {
-      const { customerId } = req.params;
-      const customer = await prisma.customer.findUnique({
-         where: { uuid: customerId },
-      });
+  try {
+    const { customerId } = req.params;
+    const customer = await prisma.customer.findUnique({
+      where: { uuid: customerId },
+      include: {
+        Register_Event: {
+          include: {
+            event: {
+              include: {
+                detail_event: true,
+              },
+            },
+          },
+        },
+        Special_Request: true,
+      },
+    });
 
-      if (!customer) {
-         return res.status(404).json({ error: 'Customer tidak ditemukan' });
-      }
+    if (!customer) {
+      return res.status(404).json({ error: "Customer tidak ditemukan" });
+    }
 
-      res.status(200).send({
-         status: 200,
-         message: "OK",
-         data: customer
-      });
-   } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-   }
+    res.status(200).send({
+      status: 200,
+      message: "OK",
+      data: customer,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
